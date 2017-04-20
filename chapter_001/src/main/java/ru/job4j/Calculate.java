d2a1027e66da03c3535cf4fb68708d351c9f47a2
maarulav <maarulav.fl@gmail.com>
2017-04-20 10:37:07 +0300

    Added modified files

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j;
 
+/**
+*Класс для вычисления
+*В данный момент пуст
+*/
 public class  Calculate{
 
+/**
+*Основной метод в программе
+*/
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");

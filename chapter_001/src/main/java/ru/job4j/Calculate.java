25513c553afcbf4d64a9fd3c22c2704d175521d0
maarulav <maarulav.fl@gmail.com>
2017-04-21 09:55:07 +0300

    Added checkstyle.xml, modified root pom.xml and fixed some mistakes in Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,17 @@
 package ru.job4j;
 
 /**
-*Класс для вычисления
-*В данный момент пуст
+*Класс для вычисления.
+*В данный момент пуст.
 */
-public class  Calculate{
+public class  Calculate {
 
 /**
-*Основной метод в программе
+*Основной метод в программе.
 *@param args - аргументы из командной строки.
 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello World");
 	}
 }
